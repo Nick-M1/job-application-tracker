6691d7d3617e66bd3ce83220f5e038ed5a8df5ae
Nick-M1 <nicholasmalloura@gmail.com>
2023-04-25 15:36:18 +0100

    homepage

--- src/constants/assets-constants.ts
+++ src/constants/assets-constants.ts
@@ -1,8 +1,11 @@
 import logoImg from '../assets/logo.png'
 import noProfileImg from '../assets/no-profile-picture-icon.svg'
+import homepageGif from '../assets/homepage-gif.gif'
 
 export const LOGO_IMG = logoImg
 export const NO_PROFILE_IMG = noProfileImg
+export const HOMEPAGE_GIF = homepageGif
+
 export const PLACEHOLDER_IMG = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/Placeholder_view_vector.svg'
 
 export const LOCALSTORAGE_OPENAI_KEY = 'OPENAI_API_KEY'
--- src/routes/HomePageRoute.tsx
+++ src/routes/HomePageRoute.tsx
@@ -1,5 +1,19 @@
+import React from "react";
+import {HOMEPAGE_GIF} from "../constants/assets-constants";
+
 export function Component() {
     return (
-        <div>HOME</div>
+        <div className='text-center pt-10 px-3'>
+            <h3 className='text-3xl font-bold font-montserrat-bold text-white'>Job Application Website</h3>
+            <h4 className='font-semibold mb-12'>To help you manage your applications!</h4>
+
+            <ul className='list-decimal list-inside marker:text-teal-500'>
+                <li>Upload your job applications.</li>
+                <li>View your calender.</li>
+                <li>Generate dynamic job application templates using AI.</li>
+            </ul>
+
+            <img src={HOMEPAGE_GIF} alt='logo' title='Home page' className='max-h-[50dvh] max-w-[80dvw] mx-auto'/>
+        </div>
     )
 }
